13141449bc1ddf45f1bb6be87b94c3b8fa26d6d4
Prateek Tiwari <prateektiwari5399@gmail.com>
2019-06-01 19:44:29 +0530

    "Second commit"

---
+++ app/src/main/java/com/example/visitorsmanager/PhotoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.visitorsmanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo)
+    }
+}
